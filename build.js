a4969418c5a451d462f26acec329109ccfff6c51
Kyle E. Mitchell <kyle@kemitchell.com>
2017-10-16 09:10:44 -0700

    Extract from licensezero.com server

---
+++ build.js
@@ -0,0 +1,28 @@
+var data = require('iso-3166-2').data
+
+var EMBARGOES = [
+  'CU', // Cuba
+  'IR', // Iran
+  'KP', // North Korea
+  'LB', // Lebanon
+  'LY', // Libya
+  'SD', // Sudan
+  'SO', // Somalia
+  'SY' // Syria
+]
+
+console.log(JSON.stringify(
+  Object.keys(data)
+    .reduce(function (codes, country) {
+      if (EMBARGOES.includes(country)) return codes
+      return codes.concat(
+        Object.keys(data[country].sub)
+          .reduce(function (codes, subdivision) {
+            return codes.concat(subdivision)
+          }, [])
+      )
+    }, [])
+    .sort(),
+  null,
+  2
+))
